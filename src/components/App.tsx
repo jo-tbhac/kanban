17fed9bb63de20792dbbe757b8ea10b8a2d433ab
Jun <cornerstone.am3@gmail.com>
2020-05-02 15:26:07 +0900

    create main screen container

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,16 +3,24 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import SignIn from './SignIn';
 import SignUp from './SignUp';
+import Header from './Header';
+import BoardIndex from './BoardIndex';
 
-const App = () => {
-  return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Switch>
-        <Route path="/signin"><SignIn /></Route>
-        <Route path="/signup"><SignUp /></Route>
-      </Switch>
-    </BrowserRouter>
-  );
-};
+const Main = () => (
+  <div className="mainContainer">
+    <Header />
+    <BoardIndex />
+  </div>
+);
+
+const App = () => (
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Switch>
+      <Route path="/signin"><SignIn /></Route>
+      <Route path="/signup"><SignUp /></Route>
+      <Route exact path="/"><Main /></Route>
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
