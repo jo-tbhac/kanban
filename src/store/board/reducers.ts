2e6da5f68c0d3013b8c9ea3bf9ce6e8e4f2f0ea5
Jun <cornerstone.am3@gmail.com>
2020-05-05 21:57:28 +0900

    create action 'SHOW_BOARD'

--- src/store/board/reducers.ts
+++ src/store/board/reducers.ts
@@ -1,19 +1,34 @@
 import {
   SHOW_BOARD_INDEX,
+  SHOW_BOARD,
   BoardState,
   BoardActionTypes,
 } from './types';
 
-const initialState: BoardState = {
-  isIndexVisible: true,
+const initialBoardProps = {
+  id: 0,
+  title: '',
+  updatedAt: '',
+  lists: [],
 };
 
-const boardReducer = (state = initialState, action: BoardActionTypes) => {
+const initialState: BoardState = {
+  isIndexVisible: true,
+  selectedBoard: initialBoardProps,
+};
+
+const boardReducer = (state = initialState, action: BoardActionTypes): BoardState => {
   switch (action.type) {
     case SHOW_BOARD_INDEX:
       return {
         ...state,
         isIndexVisible: true,
+      };
+    case SHOW_BOARD:
+      return {
+        ...state,
+        isIndexVisible: false,
+        selectedBoard: action.payload,
       };
     default:
       return state;
--- src/store/board/types.ts
+++ src/store/board/types.ts
@@ -1,11 +1,41 @@
 export const SHOW_BOARD_INDEX = 'SHOW_BOARD_INDEX';
+export const SHOW_BOARD = 'SHOW_BOARD';
 
-export type BoardState = {
+export interface BoardState {
   isIndexVisible: boolean
+  selectedBoard: Board
 }
 
-type ShowBoardIndexAction = {
+export interface ShowBoardParams {
+  boardID: number
+}
+
+export interface Board {
+  id: number
+  title: string
+  updatedAt: string
+  lists: List[]
+}
+
+export interface List {
+  id: number
+  name: string
+  cards: Card[]
+}
+
+export interface Card {
+  id: number
+  title: string
+  description: string
+}
+
+interface ShowBoardIndexAction {
   type: typeof SHOW_BOARD_INDEX
 }
 
-export type BoardActionTypes = ShowBoardIndexAction;
+interface ShowBoardAction {
+  type: typeof SHOW_BOARD
+  payload: Board
+}
+
+export type BoardActionTypes = ShowBoardIndexAction | ShowBoardAction;
