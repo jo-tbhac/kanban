f4acb00fd92ea7cc769dcf376db098243a684544
Jun <cornerstone.am3@gmail.com>
2020-05-06 01:44:14 +0900

    create testUtils

---
+++ src/testUtils.tsx
@@ -0,0 +1,25 @@
+import React, { ReactElement } from 'react';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { render as rtlRender } from '@testing-library/react';
+
+import { rootReducer } from './store';
+
+export const storeFactory = (initialState: any = {}) => (
+  createStore(rootReducer, initialState, applyMiddleware(thunk))
+);
+
+const render = (ui: ReactElement, initialState?: any) => {
+  const store = storeFactory(initialState);
+  const Wrapper = ({ children }: any) => (
+    <Provider store={store}>
+      {children}
+    </Provider>
+  );
+
+  return rtlRender(ui, { wrapper: Wrapper });
+};
+
+export * from '@testing-library/react';
+export { render };
