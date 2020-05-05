92de58c961eed3e9e5e11951a7de21a9f493c8c6
Jun <cornerstone.am3@gmail.com>
2020-05-05 22:00:51 +0900

    mapping to component props from redux

--- src/components/BoardIndex.tsx
+++ src/components/BoardIndex.tsx
@@ -1,21 +1,42 @@
 import React from 'react';
+import { connect, ConnectedProps } from 'react-redux';
 
+import * as boardActions from '../store/board/actions';
 import dataStore from '../tmp_dataStore';
 
-const BoardIndex = () => (
-  <div className="boardIndexContainer">
-    {dataStore.map((data) => (
-      <div className="boardIndexCard" key={data.id}>
-        <div className="boardIndexCardTop">
-          <div className="boardIndexCardTop__title">{data.title}</div>
-        </div>
-        <div className="boardIndexCardBottom">
-          <div className="boardIndexCardBottom__label">Updated at</div>
-          <div>{data.updatedAt}</div>
-        </div>
-      </div>
-    ))}
-  </div>
-);
+const mapDispatchToProps = {
+  showBoard: boardActions.showBoard,
+};
 
-export default BoardIndex;
+const connector = connect(null, mapDispatchToProps);
+
+type PropsFromRedux = ConnectedProps<typeof connector>
+
+const BoardIndex = (props: PropsFromRedux) => {
+  const { showBoard } = props;
+
+  return (
+    <div className="boardIndexContainer">
+      {dataStore.map((data) => (
+        <div
+          className="boardIndexCard"
+          key={data.id}
+          onClick={showBoard}
+          onKeyPress={showBoard}
+          role="button"
+          tabIndex={0}
+        >
+          <div className="boardIndexCardTop">
+            <div className="boardIndexCardTop__title">{data.title}</div>
+          </div>
+          <div className="boardIndexCardBottom">
+            <div className="boardIndexCardBottom__label">Updated at</div>
+            <div>{data.updatedAt}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default connector(BoardIndex);
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { connect, ConnectedProps } from 'react-redux';
 
-const Header = () => (
-  <div className="header">
-    <div className="headerIcon">
-      <FontAwesomeIcon icon={['fas', 'bars']} className="headerIcon__icon" />
+import * as boardActions from '../store/board/actions';
+
+const mapDispatchToProps = {
+  showBoardIndex: boardActions.showBoardIndex,
+};
+
+const connector = connect(null, mapDispatchToProps);
+
+type PropsFromRedux = ConnectedProps<typeof connector>
+
+const Header = (props: PropsFromRedux) => {
+  const { showBoardIndex } = props;
+
+  return (
+    <div className="header">
+      <div className="headerIcon">
+        <FontAwesomeIcon
+          icon={['fas', 'home']}
+          className="headerIcon__icon"
+          onClick={showBoardIndex}
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
-export default Header;
+export default connector(Header);
