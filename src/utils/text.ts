ce34bdd190e9531516de6dd0a3d9f7e23a4bec65
Jun <cornerstone.am3@gmail.com>
2020-06-10 13:37:52 +0900

    add text for component

---
+++ src/utils/text.ts
@@ -0,0 +1,9 @@
+// dialog
+export const internalServerErrorTitle = '通信に失敗しました';
+export const internalServerErrorDescription = 'サーバーが応答を停止しています';
+export const badRequestDescription = 'リクエストが不正です';
+export const cancelButtonText = 'キャンセル';
+export const confirmButtonText = '確認';
+
+// Session
+export const failedSignUpTitle = 'サインアップに失敗しました';
