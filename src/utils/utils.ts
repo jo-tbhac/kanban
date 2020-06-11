92449ce213588e2f7af9282f92fff32bcdfb3f02
Jun <cornerstone.am3@gmail.com>
2020-06-11 18:23:35 +0900

    add function for format dateString

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,9 @@
-// eslint-disable-next-line import/prefer-default-export
 export const joinErrors = (errors: {text: string}[]) => {
   const errorStrings = errors.map((error) => error.text);
   return errorStrings.join('\n');
+};
+
+export const formatRFC3339DateString = (dateString: string) => {
+  const splitDateString = dateString.split('T');
+  return splitDateString[0].replace(/-/g, '/');
 };
