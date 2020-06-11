9066ba3604b9b8dc69acb27a4684fd75374f88ce
Jun <cornerstone.am3@gmail.com>
2020-06-11 19:30:51 +0900

    add test for fetch board data

--- src/__tests__/components/BoardIndex.test.tsx
+++ src/__tests__/components/BoardIndex.test.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { render, fireEvent, storeFactory } from '../../testUtils';
 
 import BoardIndex from '../../components/BoardIndex';
+import { mockBoards } from '../../utils/mockData';
 
 describe('<BoardIndex />', () => {
   test('update state of `board.isIndexVisible` to `false` when click a board index card', () => {
-    const store = storeFactory();
+    const store = storeFactory({ board: { boards: mockBoards } });
     const { getAllByTestId } = render(<BoardIndex />, store);
     fireEvent.click(getAllByTestId('boardIndexCard')[0]);
     const newState = store.getState().board;
--- src/__tests__/store/board/actions.test.ts
+++ src/__tests__/store/board/actions.test.ts
@@ -1,12 +1,19 @@
+import MockAdapter from 'axios-mock-adapter';
+import axios from 'axios';
+
 import { SHOW_BOARD_INDEX } from '../../../store/board/types';
-import { showBoard, showBoardIndex } from '../../../store/board/actions';
+import { showBoard, showBoardIndex, fetchAllBoards } from '../../../store/board/actions';
 import { storeFactory } from '../../../testUtils';
 import dataStore from '../../../tmp_dataStore';
+import { mockBoards } from '../../../utils/mockData';
 
 describe('board actions', () => {
   let store: any;
+  let mock: MockAdapter;
+
   beforeEach(() => {
     store = storeFactory();
+    mock = new MockAdapter(axios);
   });
 
   test('returns an action with type `SHOW_BOARD_INDEX`', () => {
@@ -22,4 +29,26 @@ describe('board actions', () => {
         expect(board.selectedBoard).toEqual(dataStore[0]);
       })
   ));
+
+  test('returns state `boards` that recieved form server upon dispatch an action `fetchAllBoards` is successful', () => {
+    const responseData = { boards: mockBoards };
+    mock.onGet('/boards').reply(200, responseData);
+
+    store.dispatch(fetchAllBoards())
+      .then(() => {
+        const { board } = store.getState();
+        expect(board.boards).toEqual(mockBoards);
+      });
+  });
+
+  test('returns state `boards` that recieved form server upon dispatch an action `fetchAllBoards` is successful', () => {
+    const responseData = { boards: mockBoards };
+    mock.onGet('/boards').reply(200, responseData);
+
+    store.dispatch(fetchAllBoards())
+      .then(() => {
+        const { board } = store.getState();
+        expect(board.boards).toEqual(mockBoards);
+      });
+  });
 });
