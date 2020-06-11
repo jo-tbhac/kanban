82906e7e633d6d69c848f163eeb5f6a34dcc48bc
Jun <cornerstone.am3@gmail.com>
2020-06-11 22:55:16 +0900

    add test for react router

--- src/__tests__/components/BoardIndex.test.tsx
+++ src/__tests__/components/BoardIndex.test.tsx
@@ -1,15 +1,35 @@
-import React from 'react';
-import { render, fireEvent, storeFactory } from '../../testUtils';
+import React, { ReactElement } from 'react';
+import { MemoryRouter, Route } from 'react-router-dom';
 
+import { render, fireEvent, storeFactory } from '../../testUtils';
 import BoardIndex from '../../components/BoardIndex';
 import { mockBoards } from '../../utils/mockData';
+import { Store } from '../../store';
 
 describe('<BoardIndex />', () => {
-  test('update state of `board.isIndexVisible` to `false` when click a board index card', () => {
+  let mockLocation: {pathname: string};
+  const renderWithRouter = (component: ReactElement, store: Store) => (
+    render(
+      <MemoryRouter>
+        {component}
+        <Route
+          path="*"
+          render={({ location }) => {
+            mockLocation = location;
+            return null;
+          }}
+        />
+      </MemoryRouter>,
+      store,
+    )
+  );
+
+  test('navigate to `/board/:boardId` when click a board index card', () => {
     const store = storeFactory({ board: { boards: mockBoards } });
-    const { getAllByTestId } = render(<BoardIndex />, store);
+    const { getAllByTestId } = renderWithRouter(<BoardIndex />, store);
     fireEvent.click(getAllByTestId('boardIndexCard')[0]);
-    const newState = store.getState().board;
-    expect(newState.isIndexVisible).toBe(false);
+
+
+    expect(mockLocation.pathname).toBe(`/board/${mockBoards[0].id}`);
   });
 });
