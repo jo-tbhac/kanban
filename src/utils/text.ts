e773a5e9ddbb8cbf9a869b15827384a3d2443391
Jun <cornerstone.am3@gmail.com>
2020-06-12 14:06:53 +0900

    add text for board form

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -5,6 +5,7 @@ export const badRequestDescription = 'リクエストが不正です';
 export const unAuthorizationTitle = '認証に失敗しました';
 export const cancelButtonText = 'キャンセル';
 export const confirmButtonText = '確認';
+export const createButtonText = '作成';
 
 // Session
 export const failedSignUpTitle = 'サインアップに失敗しました';
@@ -12,3 +13,5 @@ export const failedSignInTitle = 'サインインに失敗しました';
 
 // board
 export const failedFetchBoardData = 'ボードの情報を取得できませんでした';
+export const newBoardFormTitle = 'ボードを新規作成';
+export const boardNameFormPlaceholder = 'ボードのタイトルを入力';
