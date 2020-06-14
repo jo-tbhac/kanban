bd35806994d7d2f3681ab3a1b8591a0eb433a449
Jun <cornerstone.am3@gmail.com>
2020-06-14 19:37:14 +0900

    inplement toggle label edit component

--- src/components/LabelEdit.tsx
+++ src/components/LabelEdit.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, Dispatch, SetStateAction } from 'react';
 import { connect, ConnectedProps } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { CSSTransition } from 'react-transition-group';
 
 import { RootState } from '../store';
 import LabelEditRow from './LabelEditRow';
+import { editLabelHeaderText, newLabelButtonText } from '../utils/text';
 
 const mapStateToProps = (state: RootState) => {
   const { label } = state;
@@ -15,26 +17,44 @@ const mapStateToProps = (state: RootState) => {
 const connector = connect(mapStateToProps);
 
 type PropsFromRedux = ConnectedProps<typeof connector>
+type LabelEditProps = PropsFromRedux&{
+  setLabelEditVisible: Dispatch<SetStateAction<boolean>>
+}
 
-const LabelEdit = (props: PropsFromRedux) => {
-  const { labels } = props;
+const LabelEdit = (props: LabelEditProps) => {
+  const { labels, setLabelEditVisible } = props;
+  const [inProp, setInProp] = useState(true);
 
   return (
-    <div className="labelEditContainer">
-      <div className="labelEditHeader">
-        <div className="labelEditCloseButton">
-          <FontAwesomeIcon icon={['fas', 'times']} className="labelEditCloseButton__icon" />
+    <CSSTransition
+      in={inProp}
+      appear
+      timeout={300}
+      onExited={() => setLabelEditVisible(false)}
+      classNames="labelEditContainer"
+    >
+      <div className="labelEditContainer">
+        <div className="labelEditHeader">
+          <div
+            role="button"
+            tabIndex={0}
+            onClick={() => setInProp(false)}
+            onKeyPress={() => setInProp(false)}
+            className="labelEditCloseButton"
+          >
+            <FontAwesomeIcon icon={['fas', 'times']} className="labelEditCloseButton__icon" />
+          </div>
+          <p className="labelEditHeader__title">{editLabelHeaderText}</p>
         </div>
-        <p className="labelEditHeader__title">ラベル一覧</p>
+        <div className="labelEditIndexContainer">
+          {labels.map((label) => <LabelEditRow key={label.id} label={label} />)}
+        </div>
+        <div className="newLabelButton">
+          <FontAwesomeIcon icon={['fas', 'plus']} />
+          <p className="newLabelButton__text">{newLabelButtonText}</p>
+        </div>
       </div>
-      <div className="labelEditIndexContainer">
-        {labels.map((label) => <LabelEditRow key={label.id} label={label} />)}
-      </div>
-      <div className="newLabelButton">
-        <FontAwesomeIcon icon={['fas', 'plus']} />
-        <p className="newLabelButton__text">ラベルを新規作成</p>
-      </div>
-    </div>
+    </CSSTransition>
   );
 };
 
--- src/components/LabelIndex.tsx
+++ src/components/LabelIndex.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect, ConnectedProps } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useParams } from 'react-router-dom';
@@ -7,6 +7,7 @@ import { RootState } from '../store';
 import * as labelActions from '../store/label/actions';
 import Label from './Label';
 import LabelEdit from './LabelEdit';
+import { editLabelButtonText } from '../utils/text';
 
 const mapStateToProps = (state: RootState) => {
   const { label } = state;
@@ -25,6 +26,8 @@ type PropsFromRedux = ConnectedProps<typeof connector>
 
 const LabelIndex = (props: PropsFromRedux) => {
   const { labels, fetchAllLabel } = props;
+
+  const [isLabelEditVisible, setLabelEditVisible] = useState(false);
   const { boardID } = useParams();
 
   useEffect(() => {
@@ -40,11 +43,17 @@ const LabelIndex = (props: PropsFromRedux) => {
       <div className="labelContainer">
         {labels.map((label) => <Label key={label.id} label={label} />)}
       </div>
-      <div className="addLabelButton">
+      <div
+        role="button"
+        tabIndex={0}
+        onClick={() => setLabelEditVisible(true)}
+        onKeyPress={() => setLabelEditVisible(true)}
+        className="addLabelButton"
+      >
         <FontAwesomeIcon icon={['fas', 'plus']} className="addLabelButton__icon" />
-        <div className="addLabelButton__text">Add label</div>
+        <div className="addLabelButton__text">{editLabelButtonText}</div>
       </div>
-      <LabelEdit />
+      {isLabelEditVisible && <LabelEdit setLabelEditVisible={setLabelEditVisible} />}
     </>
   );
 };
