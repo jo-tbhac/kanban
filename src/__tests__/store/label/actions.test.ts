23c25916d597509f0bec59f054782acc024689b5
Jun <cornerstone.am3@gmail.com>
2020-06-16 12:55:04 +0900

    add test for update label action

--- src/__tests__/store/label/actions.test.ts
+++ src/__tests__/store/label/actions.test.ts
@@ -5,8 +5,8 @@ import { storeFactory } from '../../../testUtils';
 import { mockLabels, mockLabel } from '../../../utils/mockData';
 import { Store } from '../../../store';
 import { dialogTypeError } from '../../../store/dialog/types';
-import { fetchAllLabel, createLabel } from '../../../store/label/actions';
-import { failedCreateLabel } from '../../../utils/text';
+import { fetchAllLabel, createLabel, updateLabel } from '../../../store/label/actions';
+import { failedCreateLabel, failedUpdateLabel } from '../../../utils/text';
 
 describe('label actions', () => {
   let store: Store;
@@ -56,6 +56,38 @@ describe('label actions', () => {
         expect(dialog.isDialogVisible).toBeTruthy();
         expect(dialog.type).toBe(dialogTypeError);
         expect(dialog.title).toBe(failedCreateLabel);
+        expect(dialog.description).toBe('some error...');
+      });
+  });
+
+  test('returns state `labels` that updated one record upon dispatch an action `updateLabel` is successful', () => {
+    store = storeFactory({ label: { labels: mockLabels } });
+    const params = { name: 'updated', color: '#222' };
+    const responseData = { label: { id: 1, ...params } };
+    const labelID = 1;
+    mock.onPatch(`/label/${labelID}`).reply(200, responseData);
+
+    return store.dispatch(updateLabel(labelID, params) as any)
+      .then(() => {
+        const { label } = store.getState();
+        const updatedLabel = label.labels.find((l) => l.id === responseData.label.id);
+        expect(updatedLabel?.name).toBe(params.name);
+        expect(updatedLabel?.color).toBe(params.color);
+      });
+  });
+
+  test('returns state of dialogProps upon dispatch an action `updateLabel` and recieved status 400 from server', () => {
+    const params = { name: 'label', color: '#fff' };
+    const responseData = { errors: [{ text: 'some error...' }] };
+    const labelID = 1;
+    mock.onPatch(`/label/${labelID}`).reply(400, responseData);
+
+    return store.dispatch(updateLabel(labelID, params) as any)
+      .then(() => {
+        const { dialog } = store.getState();
+        expect(dialog.isDialogVisible).toBeTruthy();
+        expect(dialog.type).toBe(dialogTypeError);
+        expect(dialog.title).toBe(failedUpdateLabel);
         expect(dialog.description).toBe('some error...');
       });
   });
