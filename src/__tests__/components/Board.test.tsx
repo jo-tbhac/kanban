7be83b1ba34892b77ea85578b80a7bd1de1680d7
Jun <cornerstone.am3@gmail.com>
2020-06-18 14:57:53 +0900

    add test for useEffect to Board component

--- src/__tests__/components/Board.test.tsx
+++ src/__tests__/components/Board.test.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import { renderWithRouter, fireEvent, storeFactory } from '../../testUtils';
 import { Store } from '../../store';
-import Board from '../../components/Board';
+import { mockBoard } from '../../utils/mockData';
+import { Board } from '../../components/Board';
 
 describe('<Board>', () => {
+  let fetchBoard: jest.Mock;
   let store: Store;
   beforeEach(() => {
+    fetchBoard = jest.fn();
     store = storeFactory();
   });
 
-  test('should show `ListForm` when state of `isListFormVisible` is true', () => {
-    const { queryByTestId, getByTestId } = renderWithRouter(<Board />, store);
+  test('should show `ListForm` when state of `isListsFormVisible` is true', () => {
+    const { queryByTestId, getByTestId } = renderWithRouter(
+      <Board selectedBoard={mockBoard} fetchBoard={fetchBoard} />,
+      store,
+    );
     fireEvent.click(getByTestId('addListButton'));
 
     expect(getByTestId('listForm')).toBeVisible();
@@ -19,9 +26,34 @@ describe('<Board>', () => {
   });
 
   test('should hide `ListForm` when state of `isListFormVisible` is false', () => {
-    const { queryByTestId, getByTestId } = renderWithRouter(<Board />, store);
+    const { queryByTestId, getByTestId } = renderWithRouter(
+      <Board selectedBoard={mockBoard} fetchBoard={fetchBoard} />,
+      store,
+    );
 
     expect(getByTestId('addListButton')).toBeVisible();
     expect(queryByTestId('listForm')).toBeNull();
+  });
+
+  test('should call `fetchBoard` when component did mount', () => {
+    renderWithRouter(
+      <Route path="/board/:boardID">
+        <Board selectedBoard={mockBoard} fetchBoard={fetchBoard} />
+      </Route>,
+      store,
+      ['/board/1'],
+    );
+    expect(fetchBoard).toHaveBeenCalled();
+  });
+
+  test('should not call `fetchBoard` if url params `boardID` is not a number when component did mount', () => {
+    renderWithRouter(
+      <Route path="/board/:boardID">
+        <Board selectedBoard={mockBoard} fetchBoard={fetchBoard} />
+      </Route>,
+      store,
+      ['/board/test'],
+    );
+    expect(fetchBoard).not.toHaveBeenCalled();
   });
 });
