d85271ff683abf5921eaeab74a4559a3b5ac60df
Jun <cornerstone.am3@gmail.com>
2020-06-26 14:41:29 +0900

    add test for deleteCard action

--- src/__tests__/store/card/actions.test.ts
+++ src/__tests__/store/card/actions.test.ts
@@ -5,8 +5,8 @@ import { storeFactory } from '../../../testUtils';
 import { mockCard, mockListsWithCard } from '../../../utils/mockData';
 import { Store } from '../../../store';
 import { dialogTypeError } from '../../../store/dialog/types';
-import { failedCreateCard, failedUpdateCard } from '../../../utils/text';
-import { createCard, updateCard } from '../../../store/card/actions';
+import { failedCreateCard, failedUpdateCard, failedDeleteCard } from '../../../utils/text';
+import { createCard, updateCard, deleteCard } from '../../../store/card/actions';
 
 describe('card actions', () => {
   let store: Store;
@@ -122,6 +122,47 @@ describe('card actions', () => {
         expect(dialog.isDialogVisible).toBeTruthy();
         expect(dialog.type).toBe(dialogTypeError);
         expect(dialog.title).toBe(failedUpdateCard);
+        expect(dialog.description).toBe('some error...');
+      });
+  });
+
+  test('returns state of `selectedBoard.lists.cards` that deleted one record`s description upon dispatch an action `deleteCard` is successful', () => {
+    const cardId = 1;
+    const listId = 1;
+    mock.onDelete(`/card/${cardId}`).reply(200);
+
+    const previousState = store.getState().board;
+    const previousList = previousState.selectedBoard.lists.find((list) => list.id === listId);
+
+    if (previousList === undefined) {
+      return expect(previousList).not.toBeUndefined();
+    }
+
+    return store.dispatch(deleteCard(cardId, listId) as any)
+      .then(() => {
+        const { board } = store.getState();
+        const targetList = board.selectedBoard.lists.find((list) => list.id === listId);
+
+        if (targetList === undefined) {
+          expect(targetList).not.toBeUndefined();
+        } else {
+          expect(targetList.cards.length).toBe(previousList.cards.length - 1);
+        }
+      });
+  });
+
+  test('returns state of dialogProps upon dispatch an action `deleteCard` and recieved status 400 from server', () => {
+    const cardId = 1;
+    const listId = 1;
+    const responseData = { errors: [{ text: 'some error...' }] };
+    mock.onDelete(`/card/${cardId}`).reply(400, responseData);
+
+    return store.dispatch(deleteCard(cardId, listId) as any)
+      .then(() => {
+        const { dialog } = store.getState();
+        expect(dialog.isDialogVisible).toBeTruthy();
+        expect(dialog.type).toBe(dialogTypeError);
+        expect(dialog.title).toBe(failedDeleteCard);
         expect(dialog.description).toBe('some error...');
       });
   });
