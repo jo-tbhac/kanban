3053c13643a2e011d6562699cba7a54c0ecaaceb
Jun <cornerstone.am3@gmail.com>
2020-06-28 22:44:55 +0900

    create component CardLabelButton

---
+++ src/components/CardLabelFormButton.tsx
@@ -0,0 +1,30 @@
+import React, { useState, useCallback } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import CardLabelForm from './CardLabelForm';
+
+const CardLabelFormButton = () => {
+  const [isCardLabelFormVisible, setCardLabelFormVisible] = useState(false);
+
+  const closeCardLabelForm = useCallback(() => {
+    setCardLabelFormVisible(false);
+  }, []);
+
+  return (
+    <div className="cardLabelFormButtonWrapper">
+      <div
+        role="button"
+        tabIndex={0}
+        onClick={() => setCardLabelFormVisible(true)}
+        onKeyPress={() => setCardLabelFormVisible(true)}
+        className="cardLabelFormButton"
+      >
+        <FontAwesomeIcon icon={['fas', 'plus']} />
+      </div>
+      {isCardLabelFormVisible
+        && <CardLabelForm closeCardLabelForm={closeCardLabelForm} />}
+    </div>
+  );
+};
+
+export default CardLabelFormButton;
