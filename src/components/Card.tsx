eef2f060aad69fd49266a242d8bee8a7260dd6d0
Jun <cornerstone.am3@gmail.com>
2020-07-05 17:45:40 +0900

    implement move card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,23 +1,98 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { connect, ConnectedProps } from 'react-redux';
+import { useDrag, useDrop } from 'react-dnd';
 
-import { CardContext } from './List';
+import { DndLCard } from '../store/card/types';
+import * as cardActions from '../store/card/actions';
+import { CardContext } from './CardIndexContainer';
 import CardDetail from './CardDetail';
 import CardLabelSmall from './CardLabelSmall';
+import { dndItemType } from '../utils/utils';
 
-const Card = () => {
+const mapDisaptchToProps = {
+  moveCard: cardActions.moveCard,
+  moveCardAcrossList: cardActions.moveCardAcrossList,
+};
+
+const connector = connect(null, mapDisaptchToProps);
+
+type PropsFromRedux = ConnectedProps<typeof connector>
+
+const Card = (props: PropsFromRedux) => {
+  const { moveCard, moveCardAcrossList } = props;
+
   const card = useContext(CardContext);
 
   const [isCardDetailVisible, setCardDetailVisible] = useState(false);
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  const [obj, drop] = useDrop({
+    accept: dndItemType.CARD,
+    collect: (monitor) => monitor.getItem(),
+    canDrop: () => false,
+    hover: (item: DndLCard) => {
+      if (!ref.current || !card) {
+        return;
+      }
+      const dragIndex = item.index;
+      const dropIndex = card.index;
+      const dragListId = item.listId;
+      const dropListId = card.listId;
+
+      if (dragIndex === dropIndex) {
+        return;
+      }
+
+      if (dragListId === dropListId) {
+        moveCard({
+          dragId: item.id,
+          dropId: card.id,
+          listId: card.listId,
+        });
+        // eslint-disable-next-line no-param-reassign
+        item.index = dropIndex;
+        return;
+      }
+
+      moveCardAcrossList({
+        dragId: item.id,
+        dropId: card.id,
+        dragListId,
+        dropListId,
+      });
+      /* eslint-disable no-param-reassign */
+      item.index = dropIndex;
+      item.listId = dropListId;
+      /* eslint-enable */
+    },
+  });
+
+  const [, drag] = useDrag({
+    item: {
+      type: dndItemType.CARD,
+      index: card?.index,
+      id: card?.id,
+      listId: card?.listId,
+    },
+    collect: (monitor) => ({ isDragging: monitor.isDragging() }),
+  });
+
+  const opacity = card?.id === obj?.id && obj.type === dndItemType.CARD ? 0.2 : 1;
+  drag(ref);
+  drop(ref);
 
   return (
     <>
       <div
         data-testid="card"
+        ref={ref}
         role="button"
         tabIndex={0}
         onClick={() => setCardDetailVisible(true)}
         onKeyPress={() => setCardDetailVisible(true)}
+        style={{ opacity }}
         className="card"
       >
         <div className="cardLabelWrapper">
@@ -41,4 +116,4 @@ const Card = () => {
   );
 };
 
-export default Card;
+export default connector(Card);
