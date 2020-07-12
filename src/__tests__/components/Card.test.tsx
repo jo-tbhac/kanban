dbf8b4976d3b30e77f24f3e93cac914027a696ee
Jun <cornerstone.am3@gmail.com>
2020-07-12 12:40:52 +0900

    add test for card component

--- src/__tests__/components/Card.test.tsx
+++ src/__tests__/components/Card.test.tsx
@@ -16,12 +16,14 @@ import { CardContext } from '../../components/CardIndexContainer';
 
 describe('Card component', () => {
   let store: Store;
+  let selectedLabelIds: number[];
   let moveCard: jest.Mock;
   let moveCardAcrossList: jest.Mock;
   let updateCardIndex: jest.Mock;
 
   beforeEach(() => {
     store = storeFactory();
+    selectedLabelIds = [];
     moveCard = jest.fn();
     moveCardAcrossList = jest.fn();
     updateCardIndex = jest.fn();
@@ -32,6 +34,7 @@ describe('Card component', () => {
       <DndProvider backend={HTML5Backend}>
         <CardContext.Provider value={mockCard}>
           <Card
+            selectedLabelIds={selectedLabelIds}
             moveCard={moveCard}
             moveCardAcrossList={moveCardAcrossList}
             updateCardIndex={updateCardIndex}
@@ -50,6 +53,7 @@ describe('Card component', () => {
       <DndProvider backend={HTML5Backend}>
         <CardContext.Provider value={mockCard}>
           <Card
+            selectedLabelIds={selectedLabelIds}
             moveCard={moveCard}
             moveCardAcrossList={moveCardAcrossList}
             updateCardIndex={updateCardIndex}
@@ -67,6 +71,7 @@ describe('Card component', () => {
       <DndProvider backend={HTML5Backend}>
         <CardContext.Provider value={mockCard}>
           <Card
+            selectedLabelIds={selectedLabelIds}
             moveCard={moveCard}
             moveCardAcrossList={moveCardAcrossList}
             updateCardIndex={updateCardIndex}
@@ -74,6 +79,7 @@ describe('Card component', () => {
         </CardContext.Provider>
         <CardContext.Provider value={mockCard2}>
           <Card
+            selectedLabelIds={selectedLabelIds}
             moveCard={moveCard}
             moveCardAcrossList={moveCardAcrossList}
             updateCardIndex={updateCardIndex}
@@ -100,6 +106,7 @@ describe('Card component', () => {
       <DndProvider backend={HTML5Backend}>
         <CardContext.Provider value={mockCard}>
           <Card
+            selectedLabelIds={selectedLabelIds}
             moveCard={moveCard}
             moveCardAcrossList={moveCardAcrossList}
             updateCardIndex={updateCardIndex}
@@ -112,5 +119,88 @@ describe('Card component', () => {
     fireEvent.dragStart(getByTestId(`card-${mockCard.id}`));
     fireEvent.dragEnd(getByTestId(`card-${mockCard.id}`));
     expect(updateCardIndex).toHaveBeenCalled();
+  });
+
+  test('should have a style of `opacity: 0.2` when card is dragging', async () => {
+    const { getByTestId } = render(
+      <DndProvider backend={HTML5Backend}>
+        <CardContext.Provider value={mockCard}>
+          <Card
+            selectedLabelIds={selectedLabelIds}
+            moveCard={moveCard}
+            moveCardAcrossList={moveCardAcrossList}
+            updateCardIndex={updateCardIndex}
+          />
+        </CardContext.Provider>
+      </DndProvider>,
+      store,
+    );
+
+    await act(async () => {
+      fireEvent.dragStart(getByTestId(`card-${mockCard.id}`));
+    });
+
+    expect(getByTestId(`card-${mockCard.id}`)).toHaveStyle('opacity: 0.2');
+  });
+
+  test('should have a style of `opacity: 1` if props of `selectedLabelIds.length` is zero', () => {
+    const { getByTestId } = render(
+      <DndProvider backend={HTML5Backend}>
+        <CardContext.Provider value={mockCard}>
+          <Card
+            selectedLabelIds={selectedLabelIds}
+            moveCard={moveCard}
+            moveCardAcrossList={moveCardAcrossList}
+            updateCardIndex={updateCardIndex}
+          />
+        </CardContext.Provider>
+      </DndProvider>,
+      store,
+    );
+
+    expect(getByTestId(`card-${mockCard.id}`)).toHaveStyle('opacity: 1');
+  });
+
+  test('should have a style of `opacity: 1` if props of `selectedLabelIds` includes label ids that attached to a card', () => {
+    const targetLabelId = 1;
+    const targetCard = { ...mockCard, labels: [{ id: targetLabelId }] };
+    selectedLabelIds = [targetLabelId];
+
+    const { getByTestId } = render(
+      <DndProvider backend={HTML5Backend}>
+        <CardContext.Provider value={targetCard}>
+          <Card
+            selectedLabelIds={selectedLabelIds}
+            moveCard={moveCard}
+            moveCardAcrossList={moveCardAcrossList}
+            updateCardIndex={updateCardIndex}
+          />
+        </CardContext.Provider>
+      </DndProvider>,
+      store,
+    );
+
+    expect(getByTestId(`card-${targetCard.id}`)).toHaveStyle('opacity: 1');
+  });
+
+  test('should have a style of `opacity: 0.4` if props of `selectedLabelIds` does not include label ids that attached to a card', () => {
+    const targetCard = { ...mockCard, labels: [{ id: 1 }] };
+    selectedLabelIds = [2];
+
+    const { getByTestId } = render(
+      <DndProvider backend={HTML5Backend}>
+        <CardContext.Provider value={targetCard}>
+          <Card
+            selectedLabelIds={selectedLabelIds}
+            moveCard={moveCard}
+            moveCardAcrossList={moveCardAcrossList}
+            updateCardIndex={updateCardIndex}
+          />
+        </CardContext.Provider>
+      </DndProvider>,
+      store,
+    );
+
+    expect(getByTestId(`card-${targetCard.id}`)).toHaveStyle('opacity: 0.4');
   });
 });
