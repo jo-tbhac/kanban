3024058f4d96c9bba63178d8aed77e56cbb715ec
Jun <cornerstone.am3@gmail.com>
2020-07-19 09:06:09 +0900

    add action for search board

--- src/store/search/actions.ts
+++ src/store/search/actions.ts
@@ -2,11 +2,18 @@ import snakeCaseKeys from 'snakecase-keys';
 import camelCaseKeys from 'camelcase-keys';
 
 import { newAxios } from '../../configureAxios';
-import { SEARCH_CARD, CLEAR_SEARCH_CARD_POOL, ON_CHANGE_SEARCH_CARD_KEYWORD } from './types';
 import { AppDispatch } from '../index';
 import { joinErrors } from '../../utils/utils';
 import { dialogTypeError, DialogTypes, OPEN_DIALOG } from '../dialog/types';
-import { failedSearchCard } from '../../utils/text';
+import { failedSearchCard, failedSearchBoard } from '../../utils/text';
+import {
+  SEARCH_CARD,
+  CLEAR_SEARCH_CARD_POOL,
+  ON_CHANGE_SEARCH_CARD_KEYWORD,
+  SEARCH_BOARD,
+  CLEAR_SEARCH_BOARD_POOL,
+  ON_CHANGE_SEARCH_BOARD_KEYWORD,
+} from './types';
 
 export const searchCard = (params: { title: string, boardId: number }) => (
   async (dispatch: AppDispatch) => {
@@ -37,5 +44,34 @@ export const clearSearchCardPool = () => ({
 
 export const onChangeSearchCardKeyword = (keyword: string) => ({
   type: ON_CHANGE_SEARCH_CARD_KEYWORD,
+  payload: keyword,
+});
+
+export const searchBoard = (name: string) => async (dispatch: AppDispatch) => {
+  const axios = newAxios();
+  const response = await axios.get('/boards/search', { params: { name } });
+
+  if (response?.status === 200) {
+    const camelizedData = camelCaseKeys(response.data);
+    dispatch({ type: SEARCH_BOARD, payload: camelizedData.boardIds });
+    return;
+  }
+
+  if (response?.status === 400) {
+    const dialogProps = {
+      type: dialogTypeError as DialogTypes,
+      title: failedSearchBoard,
+      description: joinErrors(response.data.errors),
+    };
+    dispatch({ type: OPEN_DIALOG, payload: dialogProps });
+  }
+};
+
+export const clearSearchBoardPool = () => ({
+  type: CLEAR_SEARCH_BOARD_POOL,
+});
+
+export const onChangeSearchBoardKeyword = (keyword: string) => ({
+  type: ON_CHANGE_SEARCH_BOARD_KEYWORD,
   payload: keyword,
 });
