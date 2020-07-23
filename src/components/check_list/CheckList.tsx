f90bc977dc59767ed30ca9ca45c9a90349b4cd34
Jun <cornerstone.am3@gmail.com>
2020-07-23 21:34:59 +0900

    create component 'CheckList'

---
+++ src/components/check_list/CheckList.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import ButtonNoneBorder from '../common/ButtonNoneBorder';
+import * as CheckListTypes from '../../store/check_list/types';
+import { addCheckListItem, deleteText } from '../../utils/text';
+
+type CheckListProps = {
+  checkList: CheckListTypes.CheckList
+}
+
+const CheckList = (props: CheckListProps) => {
+  const { checkList } = props;
+
+  return (
+    <div className="checkList">
+      <div className="checkListHeader">
+        <div className="checkListHeader__label">{checkList.title}</div>
+        <ButtonNoneBorder buttonText={deleteText} onClick={() => {}} />
+      </div>
+      <ButtonNoneBorder buttonText={addCheckListItem} onClick={() => {}} />
+    </div>
+  );
+};
+
+export default CheckList;
--- src/utils/text.ts
+++ src/utils/text.ts
@@ -70,3 +70,7 @@ export const checkListButtonText = 'チェックリスト';
 export const newCheckListText = 'チェックリストを作成';
 export const checkListTitleLabel = 'タイトル';
 export const failedCreateCheckList = 'チェックリストを作成できませんでした';
+export const addCheckListItem = 'アイテムを追加';
+
+// common
+export const deleteText = '削除';
