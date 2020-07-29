b898b1e05e11b89ab73cdf956640e70d3b718db5
Jun <cornerstone.am3@gmail.com>
2020-07-29 20:27:30 +0900

    implement delete check list item

--- src/components/check_list_item/CheckListItem.tsx
+++ src/components/check_list_item/CheckListItem.tsx
@@ -1,26 +1,54 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { connect, ConnectedProps } from 'react-redux';
 
 import Checkbox from './Checkbox';
 import CheckListItemName from './CheckListItemName';
-import * as checkListTypes from '../../store/check_list_item/types';
+import * as checkListItemTypes from '../../store/check_list_item/types';
+import { dialogTypeAsk } from '../../store/dialog/types';
+import * as checkListItemActions from '../../store/check_list_item/actions';
+import * as dialogActions from '../../store/dialog/actions';
+import { askCheckListItemDeleteDialog } from '../../utils/text';
 
-type CheckListItemProps = {
-  item: checkListTypes.CheckListItem
+const mapDispatchToProps = {
+  deleteCheckListItem: checkListItemActions.deleteCheckListItem,
+  openDialog: dialogActions.openDialog,
+};
+
+const connector = connect(null, mapDispatchToProps);
+
+type PropsFromRedux = ConnectedProps<typeof connector>
+
+type CheckListItemProps = PropsFromRedux & {
+  item: checkListItemTypes.CheckListItem
 }
 
 const CheckListItem = (props: CheckListItemProps) => {
-  const { item } = props;
+  const { item, deleteCheckListItem, openDialog } = props;
+
+  const onClickDelete = () => {
+    openDialog({
+      type: dialogTypeAsk,
+      title: askCheckListItemDeleteDialog,
+      onConfirm: () => deleteCheckListItem(item.id, item.checkListId),
+    });
+  };
 
   return (
     <div className="checkListItem">
       <Checkbox check={item.check} itemId={item.id} checkListId={item.checkListId} />
       <CheckListItemName item={item} />
-      <div className="checkListItem__delete">
+      <div
+        role="button"
+        tabIndex={0}
+        onClick={onClickDelete}
+        onKeyPress={onClickDelete}
+        className="checkListItem__delete"
+      >
         <FontAwesomeIcon icon={['fas', 'trash-alt']} />
       </div>
     </div>
   );
 };
 
-export default CheckListItem;
+export default connector(CheckListItem);
