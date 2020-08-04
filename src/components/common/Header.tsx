21415715b0a020380b1aae8657e2d36d3edec551
Jun <cornerstone.am3@gmail.com>
2020-08-04 21:30:32 +0900

    refactor EditLabelButton

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,41 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
 
 import SearchCardForm from '../search/SearchForm';
 import DeleteBoardButton from '../board/DeleteBoardButton';
-import LabelEdit from '../label/LabelEdit';
-import { editLabelButtonText } from '../../utils/text';
+import EditLabelButton from '../label/EditLabelButton';
 
-const Header = () => {
-  const [isLabelEditVisible, setLabelEditVisible] = useState(false);
-
-  return (
-    <div className="header">
-      <div className="headerIcon">
-        <Link to="/" className="headerIcon__toIndex">
-          <FontAwesomeIcon icon={['fas', 'home']} data-testid="homeIcon" />
-        </Link>
-      </div>
-
-      <SearchCardForm />
-
-      <DeleteBoardButton />
-
-      <div
-        data-testid="openLabelEditButton"
-        role="button"
-        tabIndex={0}
-        onClick={() => setLabelEditVisible(true)}
-        onKeyPress={() => setLabelEditVisible(true)}
-        className="openLabelEditButton"
-      >
-        <FontAwesomeIcon icon={['fas', 'pen']} />
-        <div className="openLabelEditButton__text">{editLabelButtonText}</div>
-      </div>
-      {isLabelEditVisible && <LabelEdit setLabelEditVisible={setLabelEditVisible} />}
+const Header = () => (
+  <div className="header">
+    <div className="headerIcon">
+      <Link to="/" className="headerIcon__toIndex">
+        <FontAwesomeIcon icon={['fas', 'home']} data-testid="homeIcon" />
+      </Link>
     </div>
-  );
-};
+
+    <SearchCardForm />
+    <DeleteBoardButton />
+    <EditLabelButton />
+  </div>
+);
 
 export default Header;
---
+++ src/components/label/EditLabelButton.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useRouteMatch } from 'react-router-dom';
+
+import LabelEdit from './LabelEdit';
+import { editLabelButtonText } from '../../utils/text';
+
+
+const EditLabelButton = () => {
+  const [isLabelEditVisible, setLabelEditVisible] = useState(false);
+
+  const match = useRouteMatch('/board/:boardId');
+
+  if (!match) {
+    return null;
+  }
+
+  return (
+    <>
+      <div
+        data-testid="openLabelEditButton"
+        role="button"
+        tabIndex={0}
+        onClick={() => setLabelEditVisible(true)}
+        onKeyPress={() => setLabelEditVisible(true)}
+        className="editLabelButton"
+      >
+        <FontAwesomeIcon icon={['fas', 'pen']} />
+        <div className="editLabelButton__text">{editLabelButtonText}</div>
+      </div>
+      {isLabelEditVisible && <LabelEdit setLabelEditVisible={setLabelEditVisible} />}
+    </>
+  );
+};
+
+export default EditLabelButton;
