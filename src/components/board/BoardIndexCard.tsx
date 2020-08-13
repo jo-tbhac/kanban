baa392e08de8652fa4a6d9c9f7b32eed11c4a327
Jun <cornerstone.am3@gmail.com>
2020-08-13 21:33:22 +0900

    fix board card layout

--- src/components/board/BoardIndexCard.tsx
+++ src/components/board/BoardIndexCard.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { Board } from '../../store/board/types';
-import { formatRFC3339DateString } from '../../utils/utils';
 
 type BoardIndexCardProps = {
   board: Board
@@ -13,13 +12,7 @@ const BoardIndexCard = (props: BoardIndexCardProps) => {
 
   return (
     <Link to={`/board/${board.id}`} key={board.id} className="boardIndexCard" data-testid="boardIndexCard">
-      <div className="boardIndexCardTop">
-        <div className="boardIndexCardTop__title">{board.name}</div>
-      </div>
-      <div className="boardIndexCardBottom">
-        <div className="boardIndexCardBottom__label">Updated at</div>
-        <div>{formatRFC3339DateString(board.updatedAt)}</div>
-      </div>
+      <div className="boardIndexCard__title">{board.name}</div>
     </Link>
   );
 };
